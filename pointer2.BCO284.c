157ae0de882cd45188700ee7513be042efa41913
JenilHirani01 <jenilhirani328@gmail.com>
2025-09-29 10:42:52 +0530

    Add files via upload

---
+++ pointer2.BCO284.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],*p,i;
+    p=a;
+    printf("\nEnter 5 different values.\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",p);
+    p++;
+    }
+    printf("now putting all values of array a\n");
+    printf("using pointer variable p.\n");
+    p=a;
+      for(i=0;i<5;i++)
+    {
+        printf("%d ",p[1]);
+        p++;
+    }
+    return 0;
+
+
+}
